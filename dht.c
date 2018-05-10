a8c1f7ee16fe3a751d4e583215d771a60a7f16b7
Hong Jae Lee <blcocas@naver.com>
2018-05-10 15:50:49 +0900

    new verstion

--- dht.c
+++ dht.c
@@ -6,57 +6,51 @@
 #define MAXTIMINGS    85
 #define DHTPIN        7  //wPi pin. physical num 7
 int dht11_dat[5] = { 0, 0, 0, 0, 0 };
- 
-void read_dht11_dat()
-{
-    uint8_t laststate    = HIGH;
-    uint8_t counter        = 0;
-    uint8_t j        = 0, i;
-    float    f; 
- 
+
+void read_dht11_dat() {
+    uint8_t laststate = HIGH;
+    uint8_t counter = 0;
+    uint8_t j = 0, i;
+    float f;
+
     dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
- 
-    pinMode( DHTPIN, OUTPUT );
-    digitalWrite( DHTPIN, LOW );
-    delay( 18 );
-    digitalWrite( DHTPIN, HIGH );
-    delayMicroseconds( 40 );
-    pinMode( DHTPIN, INPUT );
- 
-    for ( i = 0; i < MAXTIMINGS; i++ )
-    {
+
+    pinMode(DHTPIN, OUTPUT);
+    digitalWrite(DHTPIN, LOW);
+    delay(18);
+    digitalWrite(DHTPIN, HIGH);
+
+    delayMicroseconds(40);
+
+    pinMode(DHTPIN, INPUT);
+
+    for (i = 0; i < MAXTIMINGS; i++) {
         counter = 0;
-        while ( digitalRead( DHTPIN ) == laststate )
-        {
+        while(digitalRead(DHTPIN) == laststate) {
             counter++;
-            delayMicroseconds( 1 );
-            if ( counter == 255 )
-            {
-                break;
-            }
+            delayMicroseconds(1);
+            if (counter ==255) break;
         }
-        laststate = digitalRead( DHTPIN );
- 
-        if ( counter == 255 )
+
+        laststate = digitalRead(DHTPIN);
+
+        if (counter == 255)
             break;
- 
-        if ( (i >= 4) && (i % 2 == 0) )
-        {
-            dht11_dat[j / 8] <<= 1;
-            if ( counter > 16 )
-                dht11_dat[j / 8] |= 1;
+
+        if ((i >= 4) && (i % 2 == 0)) {
+            dht11_dat[j/8] <<= 1;
+            if (counter > 25)       // this check is how long the pulse width is low.
+                dht11_dat[j/8] |= 1;
             j++;
         }
     }
- 
-    if ( (j >= 40) &&
-         (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
-    {
+
+    if ((j >= 40) && (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF ))) {
         f = dht11_dat[2] * 9. / 5. + 32;
-        printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
-            dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f );
-    }else  {
-        printf( "Data not good, skip\n" );
+        printf("Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
+               dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f);
+    } else {
+        printf("Data not good, skip \n");
     }
 }
  
