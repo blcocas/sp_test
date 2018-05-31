c5395bfa429ccc0e5bdb3db50872ff2e123bf723
blcocas <blcocas@naver.com>
2018-05-31 05:47:46 -0700

    demo

--- forkdemo/fork_demo.c
+++ forkdemo/fork_demo.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <time.h>
-
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
 void fork_example(){
 
   int a = 1;
@@ -15,17 +16,20 @@ void fork_example(){
     if(a == 1){
       if(!on_off) pid = fork();
       on_off = 1;
-      printf("1 이입력되었습니다. 그리고 fork되었습니다. b is (%d)\n",b);
+     // printf("1 이입력되었습니다. 그리고 fork되었습니다. b is (%d)\n",b);
       b++;
      // sleep(2);
     }
 
     if(pid == 0) {
         printf("자식프로세스 입니다. while문 탈출!\n");
+	printf("pid :  %d\n",getpid());
         break; //자식프로세스
     }
     else if(a == 0){
-          //자식프로세스 종료
+      //int ppid = getppid();
+      //strcat("kill ",ppid);
+      //system("kill    //자식프로세스 종료
       printf("자식프로세스를 종료합니다.");
     }
   }
@@ -36,8 +40,12 @@ void fork_example(){
 void main(){
   fork_example();
   int a = 0;
+  int ppid = getppid();
   while(1){
-    printf("자식프로세스 작동중... a is %d\n",a);
-    a++;
+   scanf("%d",&a);
+   if(a == 1) 
+  	 strcat("kill ", ppid);
+   printf("자식프로세스 작동중... a is %d\n",a);
+   // a++;
   }
 }
