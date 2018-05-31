48a8609f335c46faefeea1e7f5f811a38142973e
blcocas <blcocas@naver.com>
2018-05-31 04:52:46 -0700

    test

--- forkdemo/fork_demo.c
+++ forkdemo/fork_demo.c
@@ -1,22 +1,25 @@
 #include <unistd.h>
 #include <stdio.h>
-
+#include <time.h>
 
 void fork_example(){
 
-  int a = 0;
+  int a = 1;
+  int b = 0;
+  printf("input : ");
+  scanf("%d",&a);
   while(1){
-
-    scanf("%d",&a);
     int pid;
-
+    int b = 0;
     if(a == 1){
       pid = fork();
-      printf("1이입력되었습니다. 그리고 fork되었습니다.");
+      printf("1 이입력되었습니다. 그리고 fork되었습니다. b is (%d)\n",b);
+      b++;
+     // sleep(2);
     }
 
     if(pid == 0) {
-        printf("자식프로세스 입니다. while문 탈출!");
+        printf("자식프로세스 입니다. while문 탈출!\n");
         break; //자식프로세스
     }
     else if(a == 0){
@@ -30,8 +33,12 @@ void fork_example(){
 
 void main(){
   fork_example();
+  int a;
   while(1){
-    printf("자식프로세스 작동중...");
-    sleep(100);
+    printf("자식프로세스 작동중...\n");
+    printf("type your number:");
+    scanf("%d",&a);
+    printf("%d",a);
+   // sleep(2);
   }
 }
